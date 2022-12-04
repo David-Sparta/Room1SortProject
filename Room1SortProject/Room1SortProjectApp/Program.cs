94721c806d1266122650f111ba5f5e1751e63374
David-Sparta <DavidWilliams@spartaglobal.com>
2022-12-04 12:57:23 +0000

    Created the factory pattern classes. Got to figure out how to pass an array of numbers next and then user input and array based on user input. Then look at MVC pattern. Then finished.

---
+++ Room1SortProject/Room1SortProjectApp/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Room1SortProjectApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sorting App, using GitHub\n" +
+                "Collaborators: \nmdahirr\nMissMatilde\nSAlighan07\nDavid-Sparta");
+            
+        }
+    }
+}
