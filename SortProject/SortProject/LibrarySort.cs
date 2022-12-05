91098803850d8a92d6cc336002b2c545b717a446
mdahirr <m.dahir@hotmail.co.uk>
2022-12-05 14:10:20 +0000

    Library sort added

---
+++ SortProject/SortProject/LibrarySort.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortProject
+{
+    public class LibrarySort : SortAlgorithm
+    {
+        public LibrarySort() 
+        {
+            
+        }
+
+        public override int[] Sort(int[] array)
+        {
+            Array.Sort(array);
+            return array;
+        }
+    }
+}
