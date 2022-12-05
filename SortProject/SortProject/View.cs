bd4e894614ca3becc3dab75d1727d019196d1bf7
MissMatilde <78206681+MissMatilde@users.noreply.github.com>
2022-12-05 11:42:33 +0000

    Revert "Removed Spaces"
    This reverts commit 9542bbc94089f8728a842cfbbe3f718cacc3f300.

--- SortProject/SortProject/View.cs
+++ SortProject/SortProject/View.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Xml.Schema;
 
 namespace SortProject
 {
@@ -33,6 +32,11 @@ namespace SortProject
             // get user input
             int SortingMethod = GetUserInput.SortingMethod();
             int size = GetUserInput.GetSize();
+
+
+
+            
+
         }
     }
 }
