8d57c7f130257e8d83ad75c523333a8acfb6aa93
SAlighan07 <salighan@spartaglobal.com>
2022-12-05 12:30:19 +0000

    Added Tests to BubbleSort Tests

--- SortProject/SortProjectTests/BubbleSortTests.cs
+++ SortProject/SortProjectTests/BubbleSortTests.cs
@@ -9,6 +9,28 @@ namespace SortProjectTests
 {
     public class BubbleSortTests
     {
-        public void 
+        private BubbleSort _bubble;
+
+        [OneTimeSetUp]
+        public void SetUp()
+        {
+            _bubble = new BubbleSort();
+        }
+
+        [Test]
+
+        public void GivenANullArray_Sort_ThrowsAnException()
+        {
+            int[] array = null;
+            Assert.That(() => _bubble.Sort(array), Throws.TypeOf<ArgumentNullException>());
+        }
+
+        [Test]
+
+        public void GivenArray_Sort_ReturnArraySize()
+        {
+            int[] num = { 2, 4, 6, 8, 10, 12 };
+            Assert.That(_bubble.Sort(num).Length, Is.EqualTo(6));
+        }
     }
 }
