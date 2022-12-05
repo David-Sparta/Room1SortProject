20ed6c3ff963e23b126560d68ca62bfbd2e83ff1
MissMatilde <78206681+MissMatilde@users.noreply.github.com>
2022-12-05 13:55:08 +0000

    RandomGeneratorTests

---
+++ SortProject/SortProjectTests/RandomGeneratorTests.cs
@@ -0,0 +1,26 @@
+﻿using SortProject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortProjectTests
+{
+    internal class RandomGeneratorTests
+    {
+        [TestCase(3)]
+        [TestCase(1)]
+        [TestCase(0)]
+        public void GivenLength_GetRandomArray_CreatesAnArrayOfSpecifiedLength(int length)
+        {
+            Assert.That(() => RandomGenerator.GetRandomArray(length).Length == length);
+        }
+
+        [Test]
+        public void GivenInvalidLength_GetRandomArray_ThrowsOverflowException()
+        {
+            Assert.That(() => RandomGenerator.GetRandomArray(-1), Throws.TypeOf<OverflowException>());
+        }
+    }
+}
